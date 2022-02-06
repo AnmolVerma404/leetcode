fafa56f7146db2f24716a4bf94b96e2c837a6f66
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-02-06 21:55:32 +0530

    Time: 5 ms (67.79%), Space: 10 MB (91.47%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.cpp
@@ -1,29 +1,37 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& v1, vector<int>& v2) {
-        sort(v1.begin(),v1.end());        
-        sort(v2.begin(),v2.end());
-        unordered_map<int,int>mp;
-        vector<int>res;
-        for(int i = 0;i<v1.size();i++){
-            int target = v1[i];
-            int low = 0, high = v2.size() - 1;
-            while(low <= high){
-                int mid = low + (high - low) / 2;
-                if(v2[mid] == target){                                      if(find(res.begin(),res.end(),v2[mid])!=res.end()){
-                                break;
-                }
-                    res.push_back(v2[mid]);
-                }else if(v2[mid]< target){
-                    low = mid + 1;
-                }else{
-                    high = mid - 1;
-                }
+    vector<int> intersection(vector<int> &v1, vector<int> &v2)
+{
+    sort(v2.begin(), v2.end());
+    unordered_map<int, int> mp;
+    int n = v1.size(), m = v2.size();
+    for (int i = 0; i < n; i++)
+    {
+        int target = v1[i];
+        int low = 0, high = m - 1;
+        while (low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            if (v2[mid] == target)
+            {
+                mp[target]++;
+                break;
+            }
+            else if (v2[mid] < target)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
             }
         }
-        // for(auto i : mp){
-        //     res.push_back(i.first);
-        // }
-        return res;
     }
+    vector<int> res;
+    for (auto i : mp)
+    {
+        res.push_back(i.first);
+    }
+    return res;
+}
 };
