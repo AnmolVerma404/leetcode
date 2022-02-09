d6f5ac442d526463e5ad610131612d9ff4af16b2
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-02-09 16:28:30 +0530

    Time: 34 ms (34.77%), Space: 13.1 MB (73.32%) - LeetHub

--- 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
+++ 532-k-diff-pairs-in-an-array/532-k-diff-pairs-in-an-array.cpp
@@ -1,16 +1,26 @@
 class Solution {
 public:
     int findPairs(vector<int>& v, int k) {
-        set<pair<int,int>>st;
+        sort(v.begin(),v.end());
         int n = v.size(),c = 0;
+        set<pair<int,int>>st;
         for(int i = 0;i<n;i++){
-            for(int j = i+1;j<n;j++){
-                if(abs(v[i]-v[j])==k){
-                    if(st.count({v[i],v[j]})==0&&st.count({v[j],v[i]})==0){
-                        // cout<<v[i]<<" "<<v[j]<<"\n";
-                        st.insert({v[i],v[j]});
-                        c++;
+            int low = 0,high = n-1;
+            while(low<=high){
+                int mid = low + (high - low)/2;
+                int t = k+v[i];
+                if(v[mid]==t){
+                    // cout<<"Find";
+                    // cout<<v[mid]<<" "<<v[i]<<"\n";
+                    // c++;
+                    if(mid!=i){
+                        st.insert({v[mid],v[i]});
                     }
+                    break;
+                }else if(v[mid]<t){
+                    low = mid + 1;
+                }else{
+                    high = mid - 1;
                 }
             }
         }
@@ -18,6 +28,5 @@ public:
         //     cout<<i.first<<" "<<i.second<<"\n";
         // }
         return st.size();
-        return c;
     }
 };
