2c62b8a695683a9c79220649139e072f4537d94d
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-02-12 19:27:29 +0530

    Time: 140 ms (69.14%), Space: 13.9 MB (70.59%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,31 +1,42 @@
-class Solution {
+class Solution
+{
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string>st;
-        for(string word : wordList){
+    int ladderLength(string beginWord, string endWord, vector<string> &wordList)
+    {
+        unordered_set<string> st;
+        for (string word : wordList)
+        {
             st.insert(word);
         }
-        if(st.find(endWord)==st.end()) return 0;
-        queue<string>q;
+        if (st.find(endWord) == st.end())
+            return 0;
+        queue<string> q;
         q.push(beginWord);
         int depth = 0;
-        while(!q.empty()){
+        while (!q.empty())
+        {
             depth++;
             int size = q.size();
-            while(size--){
+            while (size--)
+            {
                 string curr = q.front();
                 q.pop();
-                for(int i = 0;i<curr.length();i++){
+                for (int i = 0; i < curr.size(); i++)
+                {
                     string temp = curr;
-                    for(char c = 'a';c<='z';c++){
+                    for (char c = 'a'; c <= 'z'; c++)
+                    {
                         temp[i] = c;
-                        if(curr.compare(temp)==0) {
+                        if (curr.compare(temp) == 0)
+                        {
                             continue;
                         }
-                        if(temp.compare(endWord)==0){
-                            return depth+1;
+                        if (temp.compare(endWord) == 0)
+                        {
+                            return depth + 1;
                         }
-                        if(st.find(temp)!=st.end()){
+                        if (st.find(temp) != st.end())
+                        {
                             q.push(temp);
                             st.erase(temp);
                         }
