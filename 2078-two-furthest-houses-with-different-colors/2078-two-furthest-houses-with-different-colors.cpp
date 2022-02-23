c8124a1574213de4d1d2f1e5b9ebbecb6e0569bc
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-02-23 16:36:33 +0530

    Time: 4 ms (61.98%), Space: 9 MB (66.18%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.cpp
@@ -1,14 +1,9 @@
 class Solution {
 public:
     int maxDistance(vector<int>& v) {
-        int res = 0,mx = INT_MIN,n = v.size();
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                if(i!=j && v[i]!=v[j]){
-                    mx = max(mx,abs(i-j));
-                }
-            }
-        }
-        return mx;
+        int n = v.size(), i = 0,j = n-1;
+        while(v[0]==v[j]) j--;
+        while(v[n-1]==v[i]) i++;
+        return max(j,n-i-1);
     }
 };
