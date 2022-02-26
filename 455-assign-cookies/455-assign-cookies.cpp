a9621624e32d7c4c7c8fd837bee13183ce0a8a84
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-02-26 13:28:00 +0530

    Time: 40 ms (48.69%), Space: 17.5 MB (78.89%) - LeetHub

--- 455-assign-cookies/455-assign-cookies.cpp
+++ 455-assign-cookies/455-assign-cookies.cpp
@@ -1,21 +1,18 @@
 class Solution {
 public:
     int findContentChildren(vector<int>& g, vector<int>& s) {
-        priority_queue<int,vector<int>,greater<int>>gpq;
-        priority_queue<int,vector<int>,greater<int>>spq;
-        int res = 0;
-        for(auto i : g) gpq.push(i);
-        for(auto i : s) spq.push(i);
-        int n = g.size(),m = s.size();
-        while((!gpq.empty())&&(!spq.empty())){
-            int gt = gpq.top(),st = spq.top();
-            gpq.pop();
-            spq.pop();
-            if(st>=gt) res++;
-            else{
-                gpq.push(gt);
+        sort(g.begin(),g.end());
+        sort(s.begin(),s.end());
+        int i = 0 , j = 0 , count = 0;
+        while(i<g.size() && j <s.size()){
+            if(s[j]>=g[i]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++;
             }
         }
-        return res;
+        return count;
     }
 };
