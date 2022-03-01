4008f42b79911c1d36a7eab192c9f74a617c345c
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-01 11:45:00 +0530

    Time: 6 ms (65.69%), Space: 7.7 MB (89.06%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -3,7 +3,12 @@ public:
     vector<int> countBits(int n) {
         vector<int>res(n+1);
         for(int i = 0;i<=n;i++){
-            res[i] = __builtin_popcount(i);
+            int b = i,c = 0;
+            while(b){
+                b &= (b-1);
+                c++;
+            }
+            res[i]=c;
         }
         return res;
     }
