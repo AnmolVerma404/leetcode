85ecb41f2fccbbe88c5c35c239d246e4d6db96ed
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-04 12:24:41 +0530

    Time: 28 ms (28.86%), Space: 43.2 MB (9.62%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -5,12 +5,13 @@ public:
         dp[0][0] = poured;
         for(int i = 0;i<100;i++){
             for(int j = 0;j<=i;j++){
-                if(dp[i][j]>1){
+                if(dp[i][j]>=1){
                     /*
-                        1
-                      1   1
-                    1  1  1  1
+                        5 -> poured -> after balancing 1
+                    1        1
+               0.5    1    1   0.5
                     */
+                    //This will be balanced, as the first and the last cup in the row will be filled half than other.
                     dp[i+1][j]+=(dp[i][j]-1)/2.0;
                     dp[i+1][j+1]+=(dp[i][j]-1)/2.0;
                     dp[i][j] = 1;
