7fb65f7d3451c3aa2ea5592fa329199a59676873
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-09 17:03:47 +0530

    Time: 241 ms (73.56%), Space: 78.4 MB (50.56%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.cpp
@@ -3,15 +3,15 @@ public:
     int minSetSize(vector<int>& v) {
         int n = v.size();
         unordered_map<int,int>mp;
-        for(auto i : v) mp[i]++;
+        for(auto i : v)mp[i]++;
         priority_queue<int>pq;
         for(auto i : mp) pq.push(i.second);
-        int sum = 0,i=0;
+        set<int,int>st;
+        int res = 0,i = 0;
         while(!pq.empty()){
-            // cout<<pq.top()<<" ";
-            sum+=pq.top();
+            res+=pq.top();
             i++;
-            if(sum>=n/2) return i;
+            if(res>=n/2) return i;
             pq.pop();
         }
         return 0;
