be388ed31193423c240ba6a4984e6680af415a82
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-10 14:42:03 +0530

    Time: 0 ms (100.00%), Space: 9.5 MB (68.34%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -4,7 +4,7 @@ public:
         if(v[0]>k) return k;
         int low = 0, high = v.size();
         while(low<high){
-            int mid = low + (high-low)/2;
+            int mid = low + ((high-low)>>1);
             if(v[mid]-mid-1<k)low = mid+1;
             else high = mid;
         }
