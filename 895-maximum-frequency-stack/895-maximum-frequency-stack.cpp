e0080bf48b70fc15bcae313ffbc9b67e36151255
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-19 15:14:11 +0530

    Time: 330 ms (35.92%), Space: 96.7 MB (19.97%) - LeetHub

--- 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
+++ 895-maximum-frequency-stack/895-maximum-frequency-stack.cpp
@@ -5,30 +5,23 @@ public:
     unordered_map<int,stack<int>>stackFreq;
     
     FreqStack() {
-        mxFreq = 0;        
+        mxFreq = 0;//When Constructer set mxFreq to 0        
     }
     
     void push(int val) {
-        freq[val]++;
-        if(mxFreq<freq[val]) mxFreq = freq[val];
-        stackFreq[freq[val]].push(val);
+        freq[val]++;//Increase the frequency of the int val in hash map
+        if(mxFreq<freq[val]) mxFreq = freq[val];//If mxFreq is smaller than the freq of curr int val in map update it
+        stackFreq[freq[val]].push(val);//Push the val in stack Hash Map
     }
     
     int pop() {
-        int currTop = stackFreq[mxFreq].top();
-        stackFreq[mxFreq].pop();
-        freq[currTop]--;
-        if(stackFreq[mxFreq].empty()){
+        int currTop = stackFreq[mxFreq].top();//Find the top most element with max frequency
+        stackFreq[mxFreq].pop();//Pop the mxFreq in the stack Hash Map
+        freq[currTop]--;//Decrement the freq as the mxFreq is popped
+        if(stackFreq[mxFreq].empty()){//If no element of max frequency then decrement the max frequency and erase the mxFreq stack for just in case
             stackFreq.erase(mxFreq);
             mxFreq--;
         };
         return currTop;
     }
 };
-
-/**
- * Your FreqStack object will be instantiated and called as such:
- * FreqStack* obj = new FreqStack();
- * obj->push(val);
- * int param_2 = obj->pop();
- */
