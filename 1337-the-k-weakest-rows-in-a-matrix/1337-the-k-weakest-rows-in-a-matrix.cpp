e4d0bce78cb3f4c0ac6c1e04dd070703d6025a3a
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-03-27 16:09:49 +0530

    Time: 12 ms (83.53%), Space: 10.6 MB (42.57%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,15 +1,15 @@
+typedef pair<int,int> pi;
 class Solution {
 public:
     vector<int> kWeakestRows(vector<vector<int>>& m, int k) {
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        priority_queue<pi,vector<pi>,greater<pi>>pq;
         int r = m.size();
         int c = m[0].size();
         for(int i = 0;i<r;i++){
             int t = 0;
             for(int j = 0;j<c;j++){
-                if(m[i][j]==1)t++;
+                m[i][j]==1?t++:0;
             }
-            cout<<t<<" ";
             pq.push({t,i});
         }
         vector<int>res;
