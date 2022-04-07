4aed90f7e5847eb2ed931c220829197524807d9c
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-04-07 11:56:40 +0530

    Time: 3 ms (55.52%), Space: 7.7 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     int lastStoneWeight(vector<int>& v) {
-        int n = v.size();
-        if(n==1) return v[0];
+        if(v.size()==1) return v[0];
         priority_queue<int>pq;
-        int ans = 0;
         for(auto i : v) pq.push(i);
+        int res = 0;
         while(!pq.empty()){
-            int top1 = pq.top();
+            int t1 = pq.top();
             pq.pop();
-            int top2 = pq.top();
+            // if(pq.size()==1) return pq.top();
+            int t2 = pq.top();
             pq.pop();
-            ans = top1 - top2;
-            // cout<<ans<<" ";
-            if(ans!=0) pq.push(ans);
-            if(pq.size()==1){
-                return pq.top();
-            }
+            int d = t1-t2;
+            if(d!=0) pq.push(d);
+            if(pq.size()==1) return pq.top();
         }
-        // cout<<pq.size();
-        return ans;
+        return res;
     }
 };
