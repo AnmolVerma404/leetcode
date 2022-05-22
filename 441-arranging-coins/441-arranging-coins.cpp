c62a9d545e070f60441ee5c707e82f074c40c04a
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-05-22 19:59:11 +0530

    Time: 8 ms (42.43%), Space: 6 MB (27.71%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,11 +1,14 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int res = 0;
-       for(int i = 1;n>=0;i++){
-           res++;
-           n-=i;
-       }
-        return res-1;
+        int l = 0, h = n;
+        while(l<=h){
+            long mid = (l + h)/2;
+            long temp = mid*(mid+1)/2;
+            if(temp == n) return mid;
+            else if(temp < n) l = mid + 1;
+            else h = mid - 1;
+        }
+        return l-1;
     }
 };
