1479c5bf54204fb31e87fae42c0034a840fc1262
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-05-23 19:47:59 +0530

    Added solution - LeetHub

--- Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
+++ Detect cycle in an undirected graph - GFG/detect-cycle-in-an-undirected-graph.cpp
@@ -6,37 +6,35 @@ using namespace std;
 class Solution {
   public:
     // Function to detect cycle in an undirected graph.
-    bool isCycle(int n, vector<int> g[]) {
-    int visited[n];
-    memset(visited, false, sizeof(visited));
-    for (int i = 0; i < n; i++)
+    bool cycleDetection(vector<int> g[], bool visited[], int i,int p)
     {
-        if (!visited[i])
+        visited[i] = true;
+        for (auto j : g[i])
         {
-            queue<pair<int, int>> q;
-            q.push({i, -1});
-            while (!q.empty())
+            if (!visited[j])
             {
-                int node = q.front().first;
-                int par = q.front().second;
-                visited[node] = true;
-                q.pop();
-                for (auto j : g[node])
-                {
-                    if (!visited[j])
-                    {
-                        q.push({j, node});
-                        visited[j] = true;
-                    }
-                    else if (par != j)
-                    {
-                        return true;
-                    }
+                if(cycleDetection(g, visited, j, i)){
+                    return true;
+                }
+            }else if(p!=j){
+                return true;
+            }
+        }
+        return false;
+    }
+    bool isCycle(int n, vector<int> g[]) {
+        bool visited[n];
+        memset(visited, false, sizeof(visited));
+        for (int i = 0; i < n; i++)
+        {
+            if (!visited[i])
+            {
+                if(cycleDetection(g, visited, i,-1)){
+                    return true;
                 }
             }
         }
-    }
-    return false;
+        return false;
     }
 };
 
