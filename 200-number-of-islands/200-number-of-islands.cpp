b28a49f0958bb7fc2232189221d16b33c4d7e8fa
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-05-27 16:22:21 +0530

    Time: 57 ms (31.08%), Space: 12.4 MB (47.03%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -3,17 +3,17 @@ public:
     int res = 0,n = 0,m = 0;
     void dfs(vector<vector<char>>&g,vector<vector<bool>>&visited,int i,int j){
         visited[i][j] = true;
-        if(i-1>=0 && visited[i-1][j] == false && g[i-1][j] == '1'){
+         if(i-1>=0 && visited[i-1][j] == false && g[i-1][j] == '1'){
             dfs(g,visited,i-1,j);
-        }
-        if(j+1<m && visited[i][j+1] == false && g[i][j+1] == '1'){
-            dfs(g,visited,i,j+1);
         }
         if(i+1<n && visited[i+1][j] == false && g[i+1][j] == '1'){
             dfs(g,visited,i+1,j);
         }
-        if(j-1>=0 && visited[i][j-1] == false && g[i][j-1] == '1'){
+		if(j-1>=0 && visited[i][j-1] == false && g[i][j-1] == '1'){
             dfs(g,visited,i,j-1);
+        }
+        if(j+1<m && visited[i][j+1] == false && g[i][j+1] == '1'){
+            dfs(g,visited,i,j+1);
         }
     }
     int numIslands(vector<vector<char>>& g) {
@@ -24,20 +24,10 @@ public:
             for(int j = 0;j<m;j++){
                 if(visited[i][j]==false && g[i][j] == '1'){
                     dfs(g,visited,i,j);
-                    // cout<<i<<" "<<j<<"\n";
                     res++;
-                    // break;
                 }
             }
-            // cout<<"E";
-            // break;
         }
-        // for(int i = 0;i<n;i++){
-        //     for(int j = 0;j<m;j++){
-        //         cout<<visited[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
         return res;
     }
 };
