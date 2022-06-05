0808bafc03ec88adfc1f9cd28355a1ac4957a461
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-05 15:08:59 +0530

    Added solution - LeetHub

---
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -0,0 +1,72 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int n, vector<vector<int>> g[], int s)
+    {
+        vector<int>distance(n,INT_MAX);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        distance[s] = 0;
+        pq.push({0,s});
+        while(!pq.empty()){
+            int dist = pq.top().first;
+            int prev = pq.top().second;
+            pq.pop();
+            for(auto i : g[prev]){
+                int next = i[0];
+                int nextDist = i[1];
+                if(distance[prev] + nextDist < distance[next]){
+                    distance[next] = distance[prev] + nextDist;
+                    pq.push({distance[next],next});
+                }
+            }
+        }
+        return distance;
+    }
+};
+
+
+// { Driver Code Starts.
+
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+        vector<vector<int>> adj[V];
+        int i=0;
+        while (i++<E) {
+            int u, v, w;
+            cin >> u >> v >> w;
+            vector<int> t1,t2;
+            t1.push_back(v);
+            t1.push_back(w);
+            adj[u].push_back(t1);
+            t2.push_back(u);
+            t2.push_back(w);
+            adj[v].push_back(t2);
+        }
+        int S;
+        cin>>S;
+        
+        Solution obj;
+    	vector<int> res = obj.dijkstra(V, adj, S);
+    	
+    	for(int i=0; i<V; i++)
+    	    cout<<res[i]<<" ";
+    	cout<<endl;
+    }
+
+    return 0;
+}
+
+  // } Driver Code Ends
