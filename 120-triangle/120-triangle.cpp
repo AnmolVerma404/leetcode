a3fbd52ca6ab1e6a3549f3c95ec3ea15ced1932c
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-13 17:47:54 +0530

    Time: 16 ms (11.39%), Space: 8.9 MB (41.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-   int minimumTotal(vector<vector<int>>& triangle) {
-	// start from level 1 till the bottom-most level. Each time determine the best path to arrive at current cell
-	for(int level = 1; level < size(triangle); level++) 
-		for(int i = 0; i <= level; i++)  // triangle[level].size() == level + 1 (level starts from 0)
-			// for the current level: 
-			triangle[level][i] += min(triangle[level - 1][min(i, (int)size(triangle[level - 1]) - 1)], // we can either come from previous level and same index
-			                          triangle[level - 1][max(i - 1, 0)]); // or the previous level and index-1
-	// finally return the last element of the bottom level
-	return *min_element(begin(triangle.back()), end(triangle.back())); 
-}
+    int minimumTotal(vector<vector<int>>& t) {
+        int n = t.size();
+        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
+        for(int i = n-1;i>=0;i--){
+            for(int j = 0;j<=i;j++){
+                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1]) + t[i][j];
+            }
+        }
+        return dp[0][0];
+    }
 };
