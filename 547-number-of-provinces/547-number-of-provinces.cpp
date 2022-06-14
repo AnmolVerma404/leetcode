fa21305a123c2e82739b77771ac8ac599228c3d0
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-14 21:08:44 +0530

    Time: 48 ms (21.35%), Space: 13.7 MB (63.96%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,32 +1,16 @@
 class Solution {
 public:
     int n = 0;
-    vector<int>trav;
-    void buildGraph(vector<vector<int>>& isCon,vector<vector<int>>& g){
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<n;j++){
-                if(isCon[i][j]!=0){
-                    if(i == j){
-                        continue;
-                    }
-                    g[i].push_back(j);
-                }
-            }
-        }
-    }
     void dfs(vector<vector<int>>&g,vector<bool>&visited,int i){
         visited[i] = true;
-        trav.push_back(i);
-        for(auto j : g[i]){
-            if(!visited[j]){
+        for(int j = 0;j<n;j++){
+            if(g[i][j] == 1 && !visited[j]){
                 dfs(g,visited,j);
             }
         }
     }
-    int findCircleNum(vector<vector<int>>& isCon) {
-        n = isCon.size();
-        vector<vector<int>>g(n);
-        buildGraph(isCon,g);
+    int findCircleNum(vector<vector<int>>& g) {
+        n = g.size();
         vector<bool>visited(n,false);
         int res = 0;
         for(int i = 0;i<n;i++){
@@ -35,14 +19,6 @@ public:
                 dfs(g,visited,i);
             }
         }
-        // for(int i = 0;i<g.size();i++){
-        //     for(int j = 0;j<g[i].size();j++){
-        //         cout<<g[i][j]<<" ";
-        //     }
-        //     cout<<"\n";
-        // }
-        // for(auto i : trav) cout<<i<<" ";
-        // cout<<"\n";
         return res;
     }
 };
