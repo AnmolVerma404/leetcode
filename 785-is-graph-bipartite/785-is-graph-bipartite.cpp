0002ad9186e60f6464f99fbfbe8a6c7c3a59e4bc
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-15 16:07:05 +0530

    Time: 27 ms (80.14%), Space: 13.4 MB (93.20%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool bipartite(vector<vector<int>>&g,vector<int>&color,int i){
-        if(color[i]==-1) color[i] = 1;
+    bool dfs(vector<vector<int>>&g,vector<int>&color,int i){
+        if(color[i] == -1) color[i] = 1;
         for(auto j : g[i]){
-            if(color[j]==-1){
+            if(color[j] == -1){
                 color[j] = 1 - color[i];
-                if(!bipartite(g,color,j)){
-                  return false;
-                 }
-            }else if(color[j] == color[i]){
+                if(!dfs(g,color,j)){
+                    return false;
+                }
+            }else if(color[i] == color[j]){
                 return false;
             }
         }
@@ -19,7 +19,7 @@ public:
         vector<int>color(n,-1);
         for(int i = 0;i<n;i++){
             if(color[i] == -1){
-                if(!bipartite(g,color,i)){
+                if(!dfs(g,color,i)){
                     return false;
                 }
             }
