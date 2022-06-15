d9019eee18ade289beb3bd9dee65d9a25921f342
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-15 14:19:25 +0530

    Time: 140 ms (77.26%), Space: 40 MB (59.78%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,34 +1,83 @@
 class Solution {
 public:
-    int networkDelayTime(vector<vector<int>>& v, int N, int k) {
-        vector<pair<int,int>>g[N+1];
-        for(int i = 0;i<v.size();i++){
-            g[v[i][0]].push_back({v[i][1],v[i][2]});
+    void buildGraph(vector<vector<int>>&times,vector<vector<pair<int,int>>>&g){
+        
+        for(int i = 0;i<times.size();i++){
+            g[times[i][0]].push_back({times[i][1],times[i][2]});
         }
-        vector<int>dist(N+1,1e9);
-        dist[k] = 0;    priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        
+    }
+    vector<int> dijkstra(vector<vector<pair<int,int>>>&g,int n,int k){
+        vector<int>dist(n+1,INT_MAX);
+        dist[k] = 0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         pq.push({0,k});
-        pair<int,int>temp;
-        bool visit[N+1];
-        memset(visit,false,sizeof(visit));
+        
+        vector<bool>visited(n+1,false);
+        
         while(!pq.empty()){
-            temp = pq.top();
+            int mainDist = pq.top().first;
+            visited[pq.top().second] = true; 
+            
             pq.pop();
-            int u = temp.second;
-            visit[u] = true;
-            for(int i =0;i<g[u].size();i++){
-                int v = g[u][i].first;
-                int w = g[u][i].second;
-                if(visit[v] == false && dist[v]>dist[u] + w){
-                    dist[v] = dist[u] + w;
-                    pq.push({dist[v],v});
+            
+            for(auto i : g[mainDist]){
+                int next = i.first;
+                int nextDist = i.second;
+                
+                if(!visited[next] && dist[mainDist] + nextDist < dist[next]){
+                    
+                    dist[next] = dist[mainDist] + nextDist;
+                    pq.push({dist[next],next});
+                    
                 }
             }
         }
+        
+        return dist;
+    }
+    
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<pair<int,int>>>g(n+1);
+        
+         for(int i = 0;i<times.size();i++){
+            g[times[i][0]].push_back({times[i][1],times[i][2]});
+        }
+        
+        
+        vector<int>dist(n+1,1e9);
+        dist[k] = 0;
+        
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+        pq.push({0,k});
+        
+        vector<bool>visited(n+1,false);
+        
+        while(!pq.empty()){
+            int mainDist = pq.top().second;
+            visited[pq.top().second] = true; 
+            
+            pq.pop();
+            
+            for(auto i : g[mainDist]){
+                int next = i.first;
+                int nextDist = i.second;
+                
+                if(!visited[next] && dist[mainDist] + nextDist < dist[next]){
+                    
+                    dist[next] = dist[mainDist] + nextDist;
+                    pq.push({dist[next],next});
+                    
+                }
+            }
+        }
+        
         int ans = 0;
         for(int i = 1;i<dist.size();i++){
             ans = max(ans,dist[i]);
         }
+        
         if(ans == 1e9) return -1;
         return ans;
     }
