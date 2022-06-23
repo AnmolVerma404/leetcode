ecc27c9c5f4da9025b45b31b9889f04218a4ab47
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-23 12:25:02 +0530

    Time: 449 ms (29.18%), Space: 56.2 MB (85.57%) - LeetHub

--- 630-course-schedule-iii/630-course-schedule-iii.cpp
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -4,7 +4,7 @@ public:
         sort(c.begin(),c.end(),[](auto &a,auto &b){return a[1] < b[1];});
         priority_queue<int>pq;
         int total = 0;
-        for(auto it : c){
+        for(auto &it : c){
             int dur = it[0],end = it[1];
             if(dur + total <=end){
                 total += dur, pq.push(dur);
