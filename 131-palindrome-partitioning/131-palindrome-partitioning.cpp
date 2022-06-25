3b3ba2b3cf02509fa1844c1fe2dbfbba20270229
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-25 07:49:43 +0530

    Time: 158 ms (69.94%), Space: 57.5 MB (67.20%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    void solve(vector<vector<string>>&res,vector<string>&path,string s,int idx){
+        if(idx == s.size()){
+            res.push_back(path);
+            return;
+        }
+        for(int i = idx;i<s.size();++i){
+            if(isPossible(s,idx,i)){
+                path.push_back(s.substr(idx,i - idx + 1));
+                solve(res,path,s,i+1);
+                path.pop_back();
+            }
+        }
+    }
+    bool isPossible(string s,int l,int h){
+        while(l<=h){
+            if(s[l++]!=s[h--]) return false;
+        }
+        return true;
+    }
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>>res;
+        vector<string>path;
+        solve(res,path,s,0);
+        return res;
+    }
+};
