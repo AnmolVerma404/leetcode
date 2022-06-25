0b6823e0ec8131bee2177088d14eb63ccc1d759f
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-25 12:32:36 +0530

    Time: 25 ms (88.79%), Space: 13.4 MB (80.76%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool dfs(vector<vector<int>>&g,vector<int>&color,int i){
         if(color[i] == -1) color[i] = 1;
-        for(auto j : g[i]){
+        for(auto &j : g[i]){
             if(color[j] == -1){
                 color[j] = 1 - color[i];
                 if(!dfs(g,color,j)){
@@ -17,7 +17,7 @@ public:
     bool isBipartite(vector<vector<int>>& g) {
         int n = g.size();
         vector<int>color(n,-1);
-        for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;++i){
             if(color[i] == -1){
                 if(!dfs(g,color,i)){
                     return false;
