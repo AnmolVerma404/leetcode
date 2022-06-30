2bfff62b48fc91fc183b4473b39644fb7b30270c
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-06-30 20:40:03 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (94.79%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int rob(vector<int>& v) {
         int n = v.size();
+        if(n == 1) return v[0];
         int prev2 = 0;
         int prev1 = v[0];
         for(int i = 1;i<n;++i){
