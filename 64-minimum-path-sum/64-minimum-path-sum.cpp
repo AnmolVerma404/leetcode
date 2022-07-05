542ea7a4e6d1b3725a1f5f2fcb1b4022ce1e40f1
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-05 17:45:12 +0530

    Time: 8 ms (89.95%), Space: 10.2 MB (5.95%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -13,7 +13,16 @@ public:
         int n = g[0].size();
         
         vector<vector<int>>dp(m,vector<int>(n,-1));
-        
-        return func(m-1,n-1,g,dp);
+        dp[0][0] = g[0][0];
+        for(int i = 0;i<m;++i){
+            for(int j = 0;j<n;++j){
+                if(i == 0 && j == 0) continue;
+                int left = 1e6, up = 1e6;
+                if(j-1>=0) left = dp[i][j-1] + g[i][j];
+                if(i-1>=0) up = dp[i-1][j] + g[i][j];
+                dp[i][j] = min(left,up);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
