af9c1718ce5b0a65dc2df19960a75968d9d8fbd3
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-05 18:56:50 +0530

    Time: 19 ms (7.23%), Space: 9 MB (22.09%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
+    int func(int i,int j,int n,vector<vector<int>>&v,vector<vector<int>>&dp){
+        if(i == n) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int left = v[i][j] + func(i+1,j,n,v,dp);
+        int right = v[i][j] + func(i+1,j+1,n,v,dp);
+        return dp[i][j] = min(left,right);
+    }
     int minimumTotal(vector<vector<int>>& t) {
         int n = t.size();
-        vector<vector<int>>dp(n+1,vector<int>(n+1,0));
-        for(int i = n-1;i>=0;i--){
-            for(int j = 0;j<=i;j++){
-                dp[i][j] = min(dp[i+1][j],dp[i+1][j+1]) + t[i][j];
-            }
-        }
-        return dp[0][0];
+        vector<vector<int>>dp(n,vector<int>(n,-1));
+        return func(0,0,n,t,dp);
     }
 };
