f29ea6d837779b70e5b1fa478a38b74a3b6311ad
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-08 16:23:50 +0530

    Time: 148 ms (44.15%), Space: 36.4 MB (5.05%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,84 @@
+class Solution
+{
+public:
+    int n;
+    vector<int> houses;
+    vector<vector<int>> cost;
+    vector<vector<vector<int>>> dp;
+    int c;
+    int rec(int i, int last, int target)
+    {
+
+        if (target < 0)
+            return -1;
+
+        if (i == n && target != 0)
+        {
+            return -1;
+        }
+        if (i == n && target == 0)
+        {
+            return 0;
+        }
+
+        if (dp[i][last][target] != -2)
+        {
+            return dp[i][last][target];
+        }
+
+        int ans = INT_MAX;
+        bool possible = false;
+
+        if (houses[i] != 0)
+        {
+            if (houses[i] != last)
+            {
+                return dp[i][last][target] = rec(i + 1, houses[i], target - 1);
+            }
+            else
+            {
+                return dp[i][last][target] = rec(i + 1, houses[i], target);
+            }
+        }
+
+        for (int k = 0; k < c; k++)
+        {
+
+            int temp;
+            if (k + 1 != last)
+            {
+                temp = rec(i + 1, k + 1, target - 1);
+            }
+            else
+            {
+                temp = rec(i + 1, k + 1, target);
+            }
+
+            if (temp != -1)
+            {
+
+                possible = true;
+                ans = min(ans, cost[i][k] + temp);
+            }
+        }
+        if (possible)
+        {
+            return dp[i][last][target] = ans;
+        }
+        else
+        {
+            return dp[i][last][target] = -1;
+        }
+    }
+    int minCost(vector<int> &house, vector<vector<int>> &cst, int r, int col, int target)
+    {
+        cost = cst;
+        houses = house;
+        c = col;
+        n = r;
+
+        dp.resize(n + 1, vector<vector<int>>(n + 1, vector<int>(target + 1, -2)));
+
+        return rec(0, 0, target);
+    }
+};
