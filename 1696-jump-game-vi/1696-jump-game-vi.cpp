f0f88c33d440ed3cedcd70b37c9b1d9e272eb733
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-09 11:24:29 +0530

    Time: 211 ms (85.61%), Space: 80.6 MB (76.55%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.cpp
+++ 1696-jump-game-vi/1696-jump-game-vi.cpp
@@ -1,15 +1,18 @@
-#define pi pair<int,int>
 class Solution {
 public:
-    int maxResult(vector<int>& nums, int k) {
-        priority_queue<pi>pq;
-        int c = nums[0];
-        pq.push({nums[0],0});
-        for(int i = 1;i<nums.size();i++){
-            while(i-pq.top().second>k)pq.pop();
-            c = nums[i] + pq.top().first;
-            pq.push({c,i});
+    int maxResult(vector<int>& v, int k) {
+        int n = v.size();
+        vector<int>dp(n);
+        dp[0] = v[0];
+        deque<int>q{0};
+        for(int i = 1;i<n;++i){
+            if(q.front()<i-k) q.pop_front();
+            dp[i] = v[i] + dp[q.front()];
+            while(!q.empty() && dp[q.back()] <= dp[i]){
+                q.pop_back();
+            }
+            q.push_back(i);
         }
-        return c;
+        return dp.back();
     }
 };
