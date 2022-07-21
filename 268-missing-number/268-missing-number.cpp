ad3db45b0b7b6a01856a8809f9fb85e6b90ebc4e
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-21 21:07:51 +0530

    Time: 167 ms (5.06%), Space: 39 MB (5.68%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,11 +1,43 @@
 class Solution {
 public:
-    int missingNumber(vector<int>& nums) {
-        int x = 0,y = 0;
-        for(int i = 0;i<nums.size();i++){
-            x ^= nums[i];
-            y ^= (i+1);
+    void mergeSort(int s,int m,int e,vector<int>&v){
+        vector<int>temp(e-s+1);
+        
+        int i = s,j = m+1,k = 0;
+        
+        while(i<=m && j<=e){
+            if(v[i]<=v[j]){
+                temp[k++] = v[i++];
+            }else{
+                temp[k++] = v[j++];
+            }
         }
-        return x^y;
+        while(i<=m){
+            temp[k++] = v[i++];
+        }
+        while(j<=e){
+            temp[k++] = v[j++];
+        }
+        
+        for(int i = s;i<=e;++i){
+            v[i] = temp[i-s];
+        }
+    }
+    void merge(int s,int e,vector<int>&v){
+        if(s<e){
+            int m = (s+e)/2;
+            merge(s,m,v);
+            merge(m+1,e,v);
+            mergeSort(s,m,e,v);
+        }
+    }
+    int missingNumber(vector<int>& v) {
+        int n = v.size();
+        merge(0,n-1,v);
+        int i = 0;
+        for(;i<n;++i){
+            if(v[i]!=i) return i;
+        }
+        return i;
     }
 };
