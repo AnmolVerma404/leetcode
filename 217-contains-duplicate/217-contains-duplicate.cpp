d22bf3c0c0ba5e77324f5eb95d5add34634028e7
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-21 21:03:17 +0530

    Time: 219 ms (7.48%), Space: 51.1 MB (70.47%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -1,41 +1,11 @@
 class Solution {
 public:
-    void mergeSort(int s,int m,int e,vector<int>&v){
-        vector<int>temp(e-s+1);
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_map<int,int>mp;
         
-        int i = s,j = m+1,k = 0;
-        
-        while(i<=m && j<=e){
-            if(v[i]<=v[j]){
-                temp[k++] = v[i++];
-            }else{
-                temp[k++] = v[j++];
-            }
-        }
-        while(i<=m){
-            temp[k++] = v[i++];
-        }
-        while(j<=e){
-            temp[k++] = v[j++];
-        }
-        
-        for(int i = s;i<=e;++i){
-            v[i] = temp[i-s];
-        }
-    }
-    void merge(int s,int e,vector<int>&v){
-        if(s<e){
-            int m = (s+e)/2;
-            merge(s,m,v);
-            merge(m+1,e,v);
-            mergeSort(s,m,e,v);
-        }
-    }
-    bool containsDuplicate(vector<int>& v) {
-        int n = v.size();
-        merge(0,n-1,v);
-        for(int i = 1;i<n;i++){
-            if(v[i] == v[i-1]) return true;
+        for(auto &it : nums){
+            mp[it]++;
+            if(mp[it]>1) return true;
         }
         return false;
     }
