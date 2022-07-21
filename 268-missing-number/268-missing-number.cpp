48c4716468737458f97a9b7ed0628778695ca39e
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-21 21:12:05 +0530

    Time: 31 ms (45.83%), Space: 18 MB (63.93%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,43 +1,15 @@
 class Solution {
 public:
-    void mergeSort(int s,int m,int e,vector<int>&v){
-        vector<int>temp(e-s+1);
-        
-        int i = s,j = m+1,k = 0;
-        
-        while(i<=m && j<=e){
-            if(v[i]<=v[j]){
-                temp[k++] = v[i++];
-            }else{
-                temp[k++] = v[j++];
-            }
-        }
-        while(i<=m){
-            temp[k++] = v[i++];
-        }
-        while(j<=e){
-            temp[k++] = v[j++];
-        }
-        
-        for(int i = s;i<=e;++i){
-            v[i] = temp[i-s];
-        }
-    }
-    void merge(int s,int e,vector<int>&v){
-        if(s<e){
-            int m = (s+e)/2;
-            merge(s,m,v);
-            merge(m+1,e,v);
-            mergeSort(s,m,e,v);
-        }
-    }
     int missingNumber(vector<int>& v) {
         int n = v.size();
-        merge(0,n-1,v);
-        int i = 0;
-        for(;i<n;++i){
-            if(v[i]!=i) return i;
+        
+        int ans = n,mask = 0;
+        
+        for(int i = 0;i<n;++i){
+            ans ^= v[i];
+            mask ^= i;
         }
-        return i;
+        
+        return ans ^= mask;
     }
 };
