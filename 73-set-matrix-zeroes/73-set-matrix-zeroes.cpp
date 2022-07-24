9d98d31373f4ca3309fe1bfeb0b93b084c42f65d
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-24 21:35:31 +0530

    Time: 41 ms (5.70%), Space: 13.2 MB (81.69%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,7 +2,7 @@ class Solution
 {
 public:
     // x -> is the row , and y-> is the column
-    void makeZero(int x, int y, int n, int m, vector<vector<int>> &matrix)
+    void makeZeroRow(int x,int n, int m, vector<vector<int>> &matrix)
     {
         // This iteration will make all the rows of xth row zero
 
@@ -10,7 +10,9 @@ public:
         {
             matrix[x][i] = 0;
         }
-
+    }
+    void makeZeroCol(int y, int n, int m,vector<vector<int>> &matrix)
+    {
         // This iteration will set the column of the yth row to zero
 
         for (int i = 0; i < n; ++i)
@@ -18,11 +20,14 @@ public:
             matrix[i][y] = 0;
         }
     }
+
     void setZeroes(vector<vector<int>> &matrix)
     {
         int n = matrix.size();
         int m = matrix[0].size();
         vector<pair<int, int>> zeroLocation;
+        vector<bool> rowZero(n, false);
+        vector<bool> colZero(m, false);
         for (int i = 0; i < n; ++i)
         {
             for (int j = 0; j < m; ++j)
@@ -36,7 +41,19 @@ public:
         int size = zeroLocation.size();
         for (int i = 0; i < size; ++i)
         {
-            makeZero(zeroLocation[i].first, zeroLocation[i].second, n, m, matrix);
+            int row = zeroLocation[i].first;
+            int col = zeroLocation[i].second;
+            if (rowZero[row] == false)
+            {
+                makeZeroRow(row, n, m, matrix);
+                rowZero[row] = true;
+            }
+
+            if (colZero[col] == false)
+            {
+                makeZeroCol(col, n, m, matrix);
+                colZero[col] = true;
+            }
         }
     }
 };
