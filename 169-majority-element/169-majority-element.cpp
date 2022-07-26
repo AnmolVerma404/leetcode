c25793ea0e893499806ab385d9f92337370cc076
Anmol Verma <77019763+AnmolVerma404@users.noreply.github.com>
2022-07-26 20:33:21 +0530

    Time: 134 ms (5.21%), Space: 59.2 MB (12.88%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
-    int majorityElement(vector<int>& v) {
-        unordered_map<int,int>mp;
-        for(auto i : v) mp[i]++;
-        int res = 0,mx = INT_MIN;
-        for(auto i : mp) {
-            if(mx<i.second){
-                mx = i.second;
-                res = i.first;
+    void ms(int s,int m,int e,vector<int>&v){
+        vector<int>temp(e-s+1);
+        
+        int i = s,j = m+1,k = 0;
+        
+        while(i<=m && j<=e){
+            if(v[i]<v[j]){
+                temp[k++] = v[i++];
+            }else{
+                temp[k++] = v[j++];
             }
         }
-        return res;
+        
+        while(i<=m) temp[k++] = v[i++];
+        while(j<=e) temp[k++] = v[j++];
+        
+        for(i = s;i<=e;++i){
+            v[i] = temp[i-s];
+        }
+    }
+    void merge(int s,int e,vector<int>&v){
+        if(s<e){
+            int m = (s+e)/2;
+            merge(s,m,v);
+            merge(m+1,e,v);
+            ms(s,m,e,v);
+        }
+    }
+    int majorityElement(vector<int>& v) {
+        int n = v.size();
+        merge(0,n-1,v);
+        return v[n/2];
     }
 };
